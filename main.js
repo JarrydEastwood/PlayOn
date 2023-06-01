5b201f88ecdcb9017c0b23068226cfa85b1432f9
JBattishill <jbattishill91@gmail.com>
2023-06-01 10:00:13 +1000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// let modal = document.getElementById('modal');
+// let btn = document.getElementById('open-btn');
+// let button = document.getElementById('ok-btn');
+
+//  btn.onclick = function () {
+//          modal.style.display = 'block';
+//      };
+
+//      button.onclick = function () {
+//          modal.style.display = 'none';
+//      };
+
+// window.onclick = function (event) {
+// if (event.target == modal) {
+// modal.style.display = "none";
+// }
+// }
+   
+//    var myVideo = document.getElementById("video1"); 
+//    var vidCont = document.getElementById("vid1");
+   
+//    function playPause() { 
+//      if (myVideo.paused) 
+//        myVideo.play(); 
+//      else 
+//        myVideo.pause(); 
+//    } 
+   
+//    function makeBig() { 
+//        myVideo.width = 560; 
+//    } 
+   
+//    function makeSmall() { 
+//        myVideo.width = 320; 
+//    } 
+   
+//    function makeNormal() { 
+//     var element = document.getElementById("vid1");
+//     element.classList.toggle("hidden");
+//    }
