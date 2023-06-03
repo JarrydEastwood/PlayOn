a56799d646e02c14bec46ef1deb9890dac92a509
JBattishill <jbattishill91@gmail.com>
2023-06-03 18:58:52 +1000

    Setting up Carousel Base

--- main.js
+++ main.js
@@ -1,40 +1,182 @@
-// let modal = document.getElementById('modal');
-// let btn = document.getElementById('open-btn');
-// let button = document.getElementById('ok-btn');
+// Listen for Click -----------------------------------------------------------
 
-//  btn.onclick = function () {
-//          modal.style.display = 'block';
-//      };
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("slider-prev")){
+        togglePrevious();
+        togglePreviousDesc();
+        updatePrev();
+    }
+    else if(e.target.classList.contains("slider-next")){
+        toggleNext();
+        toggleNextDesc();
+        updateNext();
+    }
+})
 
-//      button.onclick = function () {
-//          modal.style.display = 'none';
-//      };
 
-// window.onclick = function (event) {
-// if (event.target == modal) {
-// modal.style.display = "none";
-// }
-// }
-   
-//    var myVideo = document.getElementById("video1"); 
-//    var vidCont = document.getElementById("vid1");
-   
-//    function playPause() { 
-//      if (myVideo.paused) 
-//        myVideo.play(); 
-//      else 
-//        myVideo.pause(); 
-//    } 
-   
-//    function makeBig() { 
-//        myVideo.width = 560; 
-//    } 
-   
-//    function makeSmall() { 
-//        myVideo.width = 320; 
-//    } 
-   
-//    function makeNormal() { 
-//     var element = document.getElementById("vid1");
-//     element.classList.toggle("hidden");
-//    }
+
+// Rotate the Course Title -----------------------------------------------------------
+
+function togglePrevious(){
+    const currentActive = document.querySelector("h3.active");
+    const currentIndex = parseInt(currentActive.dataset.index);
+    //console.log(currentIndex);
+
+    if(currentIndex - 1 >= 0){
+        const previousChild = document.querySelector(`[data-index="${currentIndex-1}"]`);
+        //console.log(previousChild)
+        currentActive.classList.remove("active");
+        previousChild.classList.add("active");
+    }
+    else{
+        const previousChild = document.querySelector(`[data-index="${4}"]`);
+        //console.log(previousChild)
+        currentActive.classList.remove("active");
+        previousChild.classList.add("active");
+    }
+}
+
+
+function toggleNext(){
+    const currentActive = document.querySelector("h3.active");
+    const currentIndex = parseInt(currentActive.dataset.index);
+    //console.log(currentIndex);
+
+    if(currentIndex + 1 <= 4){
+        const nextChild = document.querySelector(`[data-index="${currentIndex+1}"]`);
+        //console.log(nextChild)
+        currentActive.classList.remove("active");
+        nextChild.classList.add("active");
+    }
+    else{
+        const nextChild = document.querySelector(`[data-index="${0}"]`);
+        //console.log(nextChild)
+        currentActive.classList.remove("active");
+        nextChild.classList.add("active");
+    }
+}
+
+
+
+// Rotate the Course Description -----------------------------------------------------------
+
+function togglePreviousDesc(){
+  const currentActive = document.querySelector("h2.active");
+  const currentIndex = parseInt(currentActive.dataset.index);
+  //console.log(currentIndex);
+
+  if(currentIndex - 1 >= 10){
+      const previousChild = document.querySelector(`[data-index="${currentIndex-1}"]`);
+      //console.log(previousChild)
+      currentActive.classList.remove("active");
+      previousChild.classList.add("active");
+  }
+  else{
+      const previousChild = document.querySelector(`[data-index="${14}"]`);
+      //console.log(previousChild)
+      currentActive.classList.remove("active");
+      previousChild.classList.add("active");
+  }
+}
+
+
+function toggleNextDesc(){
+  const currentActive = document.querySelector("h2.active");
+  const currentIndex = parseInt(currentActive.dataset.index);
+  //console.log(currentIndex);
+
+  if(currentIndex + 1 <= 14){
+      const nextChild = document.querySelector(`[data-index="${currentIndex+1}"]`);
+      //console.log(nextChild)
+      currentActive.classList.remove("active");
+      nextChild.classList.add("active");
+  }
+  else{
+      const nextChild = document.querySelector(`[data-index="${10}"]`);
+      //console.log(nextChild)
+      currentActive.classList.remove("active");
+      nextChild.classList.add("active");
+  }
+}
+
+
+
+// Rotate the Course Images -----------------------------------------------------------
+
+function updateNext() {
+    // Set the position of each item based on the current index
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      let position = parseInt(item.style.order);
+      console.log(position);
+      if (position + 1 <= 4){
+        position ++;
+        item.style.order = position;
+        console.log(position);
+      } else {
+        item.style.order = 0;
+        console.log(position);
+      }
+
+      
+    });
+    // Apply styles based on the position of each item
+    items.forEach((item) => {
+        let position2 = parseInt(item.style.order);
+      if (position2 == 0) {
+        item.classList.add('small1');
+        item.classList.remove('small2');
+      } else if (position2 == 1) {
+        item.classList.add('big1');
+        item.classList.remove('small1');
+      } else if (position2 == 2) {
+        item.classList.add('focus');
+        item.classList.remove('big1');
+      } else if (position2 == 3) {
+        item.classList.add('big2');
+        item.classList.remove('focus');
+      } else if (position2 == 4) {
+        item.classList.add('small2');
+        item.classList.remove('big2');
+      }
+    });
+  }
+
+  function updatePrev() {
+    // Set the position of each item based on the current index
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      let position = parseInt(item.style.order);
+      console.log(position);
+      if (position - 1 >= 0){
+        position --;
+        item.style.order = position;
+        console.log(position);
+      } else {
+        item.style.order = 4;
+        console.log(position);
+      }
+
+      
+    });
+    // Apply styles based on the position of each item
+    items.forEach((item) => {
+        let position2 = parseInt(item.style.order);
+      if (position2 == 0) {
+        item.classList.add('small1');
+        item.classList.remove('big1');
+      } else if (position2 == 1) {
+        item.classList.add('big1');
+        item.classList.remove('focus');
+      } else if (position2 == 2) {
+        item.classList.add('focus');
+        item.classList.remove('big2');
+      } else if (position2 == 3) {
+        item.classList.add('big2');
+        item.classList.remove('small2');
+      } else if (position2 == 4) {
+        item.classList.add('small2');
+        item.classList.remove('small1');
+      }
+    });
+  }
